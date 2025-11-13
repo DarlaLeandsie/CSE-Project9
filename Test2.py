fbec4366d1bb37a311bef81380241129985f0cb9
Darla Leandsie <gqp24003@uconn.edu>
2025-11-13 12:54:07 -0500

    Create Test2.py

---
+++ Test2.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from library import functions
+from library.classes_9 import Budget
+import os
+
+# Main Window
+window = tk.Tk()
+
+# Sets Title and Size of Window
+window.title("BudgetBuddy")
+window.geometry("1050x600")  # Bigger window for logo + content
+
+# Runs GUI
+window.mainloop()
